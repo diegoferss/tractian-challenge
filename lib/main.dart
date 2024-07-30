948a233b998607b604012a18d62aba6adec39729
Diego Silva <diegosilva@jera.com.br>
2024-07-29 21:08:03 -0300

    [new] add theme to materialapp

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:tractian/support/styles/app_themes.dart';
 
 import 'support/utils/localize.dart';
 
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: AppThemes.theme,
       localizationsDelegates: Localization.localizationsDelegates,
       supportedLocales: Localization.supportedLocales,
       onGenerateTitle: (context) => Localize.instance.of(context).appTitle,

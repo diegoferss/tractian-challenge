56d38158af45f638868c322616df5cd05259025d
Diego Silva <diegosilva@jera.com.br>
2024-08-01 22:49:37 -0300

    [new] add status icon on expandable tile

--- lib/features/asset_tree/components/expandable_tile.dart
+++ lib/features/asset_tree/components/expandable_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tractian/features/asset_tree/models/asset.dart';
 import 'package:tractian/features/asset_tree/models/component.dart';
 
 class ExpandableTile extends StatelessWidget {
@@ -14,7 +15,20 @@ class ExpandableTile extends StatelessWidget {
     if (component.subComponents.isEmpty) {
       return ListTile(
         leading: Image.asset(component.icon),
-        title: Text(component.name),
+        title: Text(
+          component.name,
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+        ),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            if (component case Asset(:final sensorType) when sensorType == 'energy')
+              const Icon(Icons.flash_on, size: 20, color: Colors.green),
+            if (component case Asset(:final status) when status == 'alert')
+              const Icon(Icons.circle, size: 12, color: Colors.red),
+          ],
+        ),
       );
     }
 
@@ -23,7 +37,11 @@ class ExpandableTile extends StatelessWidget {
       childrenPadding: const EdgeInsets.only(left: 20),
       initiallyExpanded: true,
       leading: Image.asset(component.icon),
-      title: Text(component.name),
+      title: Text(
+        component.name,
+        maxLines: 2,
+        overflow: TextOverflow.ellipsis,
+      ),
       children: component.subComponents.map((c) {
         return ExpandableTile(component: c);
       }).toList(),
